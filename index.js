7c5a55a79b7d42ac6a17b7694068303547163d54
Disky Adit Prastio <diskyadit03@gmail.com>
2022-08-24 10:26:51 +0700

    add middleware UserRoute

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import UserRoute from './routes/UserRoutes.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(UserRoute)
 
 app.listen(3000, () => {
   console.log('server up and runing...')
