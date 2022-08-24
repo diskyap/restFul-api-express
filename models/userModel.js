9e9a9ad22a732d22ef0c6dbc9c265abf95b4b17e
Disky Adit Prastio <diskyadit03@gmail.com>
2022-08-24 10:08:32 +0700

    add user model

---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+import { Sequelize } from 'sequelize'
+import db from '../config/db.js'
+import { v4 as uuidV4 } from 'uuid'
+
+const { DataTypes } = Sequelize
+
+const User = db.define(
+  'users',
+  {
+    user_id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    freezeTableName: true,
+    timestamps: false
+  }
+)
+
+User.removeAttribute('id')
+
+export default User
+;(async () => {
+  await db.sync()
+})()
