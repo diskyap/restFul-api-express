66f376c9ea1eed0c69a1b145c629815e50a37fb1
Disky Adit Prastio <diskyadit03@gmail.com>
2022-08-25 13:32:46 +0700

    change new line router getUserById

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,6 +4,6 @@ import { getUsers, getUsersById } from '../controllers/UserController.js'
 const router = express.Router()
 
 router.get('/users', getUsers)
-router.get('/users', getUsersById)
+router.get('/users/:id', getUsersById)
 
 export default router
