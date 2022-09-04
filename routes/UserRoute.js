c2a2765a1957f9bdf562012cd9004ae243894351
diskyap <diskyadit03@gmail.com>
2022-09-04 02:35:14 -0700

    add route update user

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,7 +2,8 @@ import express from 'express'
 import {
   getUsers,
   getUsersById,
-  createUser
+  createUser,
+  updateUser
 } from '../controllers/UserController.js'
 
 const router = express.Router()
@@ -10,5 +11,6 @@ const router = express.Router()
 router.get('/users', getUsers)
 router.get('/users/:id', getUsersById)
 router.post('/users', createUser)
+router.put('/users/:id', updateUser)
 
 export default router
